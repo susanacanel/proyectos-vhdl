5180dca5fed6f6da84ae3ae28777c0ed63d77a56
Susana Canel <susanacanel@gmail.com>
2020-10-20 04:12:25 -0300

    Divisor de frecuencia, genera 200 y 1 Hz

--- secuenciales/divisor2frec.vhdl
+++ secuenciales/divisor2frec.vhdl
@@ -1,58 +1,60 @@
--- 12.10.20 ------------------- Susana Canel ------------------ divisor2frec.vhdl 
--- DIVISOR DE FRECUENCIA. GENERA UN RELOJ DE 20ms (NECESARIO PARA EL ANTIRREBOTE)
--- Y OTRO DE 1s.
+-- 19.10.20 ------------------- Susana Canel ------------------ divisor2frec.vhdl 
+-- DIVISOR DE FRECUENCIA. GENERA UN RELOJ DE 200 Hz Y OTRO DE 1 Hz.
 library ieee;   
-use ieee.std_logic_1164.all;
+use ieee.std_logic_1164.all; 
 ---------------------------------------------------------------------------------
 entity divisor2frec is
-  port (clk_i          : in  std_logic;
-        rst_i          : in  std_logic; 
-        clk1s_o        : out std_logic;   
-        clk20ms_o      : out std_logic);   
+  port (clk_i     : in  std_logic;
+        rst_i     : in  std_logic; 
+        clk1s_o   : out std_logic;  
+        clk5ms_o  : out std_logic);   
 end entity divisor2frec;
 ---------------------------------------------------------------------------------
--- LA FRECUENCIA DEL OSCILADOR DE LA PLAQUETA ES DE 24MHz, DE MANERA QUE PARA
--- OBTENER UNA DEMORA DE 20 ms HAY QUE CONTAR: 0,02s * 24000000 (1/s) = 480000
--- PARA OBTENER UNA DEMORA DE 1 s HAY QUE CONTAR: 1s * 24000000 (1/s) = 24000000
--- MAXIMO INTEGER: 2^32=2147483647;
+-- LA FRECUENCIA DEL OSCILADOR DE LA PLAQUETA ES DE 24 MHz, DE MANERA QUE PARA
+-- OBTENER UNA FRECUENCIA DE 200 Hz (PERIODO DE 5 ms), HAY QUE CONTAR: 
+-- 0,005 s * 24.000.000 (1/s) = 120.000 PULSOS, LA MITAD DE ELLOS EN "1" (60.000)
+-- Y LOS OTROS 60.000 EN "0".
+-- PARA OBTENER UNA FRECUENCIA DE 1 Hz (PERIODO DE 1s) HAY QUE CONTAR: 
+-- 1s * 24.000.000 (1/s) = 24.000.000 O SEA 12.000.000 EN "1" Y 12.000.000 EN "0"
+-- MAXIMO INTEGER: 2^32/2 = 2.147.483.647;
 
 architecture Arq of divisor2frec is
   type     estado is (inicial, contando);
-  signal   prox          : estado;
+  signal   proximo       : estado;
   signal   auxClk1s      : std_logic;
-  signal   auxClk20ms    : std_logic;
-  signal   contador1s    : integer range 0 to 24000000; 
-  signal   contador20ms  : integer range 0 to 480000; 
-  constant CUENTA1s      : integer := 24000000;
-  constant CUENTA20ms    : integer := 480000;
+  signal   auxclk5ms     : std_logic;
+  signal   contador1s    : integer range 0 to 12000000; 
+  signal   contador5ms   : integer range 0 to 60000; 
+  constant CUENTA1s      : integer := 12000000;
+  constant CUENTA5ms     : integer := 60000;
 begin
   divisor2frecuencias: 
 	process (clk_i) begin
     if rising_edge(clk_i) then
       if rst_i='1' then
-        prox <= inicial;
+        proximo <= inicial;
       else  
 
-        case prox is
+        case proximo is
           when inicial =>      
-            auxClk1s     <= '0';
-            auxClk20ms   <= '0';
-            contador20ms <= 0;
-            contador1s   <= 0;
-            prox         <= contando;
+            auxClk1s    <= '0';
+            auxclk5ms   <= '0';
+            contador5ms <= 0;
+            contador1s  <= 0;
+            proximo     <= contando;
 
           when contando =>
             
-            -- 20 milisegundos
-            if contador20ms=CUENTA20ms then            
-              auxClk20ms   <= not auxClk20ms; 
-              contador20ms <= 0; 
+            -- 5 milisegundos
+            if contador5ms>=CUENTA5ms then            
+              auxclk5ms   <= not auxclk5ms; 
+              contador5ms <= 0; 
             else
-              contador20ms <= contador20ms + 1;
+              contador5ms <= contador5ms + 1;
             end if;
 
             -- 1 segundo
-            if contador1s=CUENTA1s then      
+            if contador1s>=CUENTA1s then      
               auxClk1s   <= not auxClk1s;
               contador1s <= 0;
             else
@@ -60,16 +62,16 @@ begin
             end if;
             
           when others =>
-            prox <= inicial;
+            proximo <= inicial;
         end case;    
       end if;
     end if;
   end process divisor2frecuencias;
 
   Salidas: 
-  process (auxClk20ms, auxClk1s) is begin
-  	clk1s_o   <= auxClk1s;
-  	clk20ms_o <= auxClk20ms;
+  process (auxclk5ms, auxClk1s) is begin
+  	clk1s_o  <= auxClk1s;
+  	clk5ms_o <= auxclk5ms;
   end process Salidas;
  
 end architecture Arq;
